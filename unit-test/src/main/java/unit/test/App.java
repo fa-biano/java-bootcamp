72de4cc4abde4e85f349a85bc87cc2690dc47a8c
Fa-biano <fbiano.souza@gmail.com>
2023-09-07 19:15:18 -0300

    test: hello world with JUnit

---
+++ unit-test/src/main/java/unit/test/App.java
@@ -0,0 +1,8 @@
+package unit.test;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ unit-test/src/main/java/unit/test/Pessoa.java
@@ -0,0 +1,32 @@
+package unit.test;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Pessoa {
+    private String nome;
+    private LocalDate nascimento;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+    public void setNascimento(LocalDate idade) {
+        this.nascimento = idade;
+    }
+
+    public Pessoa(String nome, LocalDate nascimento) {
+        this.nome = nome;
+        this.nascimento = nascimento;
+    }
+    
+    public long getIdade() {
+        long idade  = ChronoUnit.YEARS.between(this.nascimento, LocalDate.now());
+        return idade;
+    }
+}
